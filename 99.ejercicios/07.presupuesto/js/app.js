d13a9e6d2a7e76c37a562d55a51b5af37375d45c
Allann Fedora <allan.barrera@proton.me>
2026-06-10 12:14:26 -0600

    Finalizacion del curso JavaScript

--- 99.ejercicios/07.presupuesto/js/app.js
+++ 99.ejercicios/07.presupuesto/js/app.js
@@ -11,8 +11,8 @@ const egresos = [
     new Egreso('Alimentos', 500.00),
     new Egreso('Estudios', 600.00),
     new Egreso('Energía Eléctrica', 38.76),
-
-    new Egreso('Ahorro', 500)
+    new Egreso('Ahorro', 500),
+    new Egreso('Comida chuchos', 125)
 ];
 
 let cargarApp = () => {
@@ -50,6 +50,9 @@ let cargarCabecero = () => {
 // INTERNACIONALIZACIÓN para cargar formatos
 const formatoMoneda = (valor) => {
     return valor.toLocaleString('es-GT', {style:'currency', currency:'GTQ', minimumFractionDigitis:2});
+    // PESO MXN return valor.toLocaleString('es-MX', {style:'currency', currency:'MXN', minimumFractionDigitis:2});
+    // EURO return valor.toLocaleString('es-ES', {style:'currency', currency:'EUR', minimumFractionDigitis:2});
+    // USD return valor.toLocaleString('en-US', {style:'currency', currency:'USD', minimumFractionDigitis:2});
 };
 
 const formatoPorcentaje = (valor) => {
@@ -74,7 +77,7 @@ const crearIngresoHTML = (ingreso) =>{
                         <div class="elemento_porcentaje">${formatoPorcentaje(ingreso.valor/totalIngresos())}</div>
                         <div class="elemento_eliminar">
                             <button class="elemento_eliminar--btn">
-                                <ion-icon name="close-circle-outline"></ion-icon>
+                                <ion-icon name="close-circle-outline" onclick='eliminarIngreso(${ingreso.idIngreso})' ></ion-icon>
                             </button>
                         </div>
                     </div>
@@ -82,6 +85,15 @@ const crearIngresoHTML = (ingreso) =>{
     return ingresoHTML;
 };
 
+const eliminarIngreso = (idIngreso) => {
+    // con esta funcion flecha le estamos indicando a la funcion findIndex que retorne el índice del objeto ingreso que coincida en ID con el parametro que estamos enviando.
+    // la función flecha actua como un ciclo for, para recorrer todos los ingresos
+    let indiceEliminar = ingresos.findIndex(ingreso => ingreso.idIngreso === idIngreso);
+
+    ingresos.splice(indiceEliminar, 1);
+
+    cargarApp();
+};
 
 const cargarEgresos = () =>{
     let egresosHTML = '';
@@ -101,10 +113,41 @@ const crearEgresoHTML = (egreso) =>{
                         <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor/totalEgresos())}</div>
                         <div class="elemento_eliminar">
                             <button class="elemento_eliminar--btn">
-                                <ion-icon name="close-circle-outline"></ion-icon>
+                                <ion-icon name="close-circle-outline" onclick='eliminarEgreso(${egreso.idEgreso})' ></ion-icon>
                             </button>
                         </div>
                     </div>
                 </div>`;
     return ingresoHTML;
+};
+
+const eliminarEgreso = (idEgreso) => {
+    let indiceEliminar = egresos.findIndex(egreso => egreso.idEgreso === idEgreso);
+
+    // splice elimina indices. si no se especifica el numero de elementos a eliminar, los elimina todos a partir del indice proporcionado.
+    egresos.splice(indiceEliminar, 1);
+
+    cargarApp();
+};
+
+let agregarDato = () => {
+    let forma = document.forms['forma'];
+
+    let tipo = forma['tipo'];
+    let descripcion = forma['descripcion'];
+    let valor = forma['valor'];
+
+    if(descripcion.value !== '' && valor.value !== ''){
+        if(tipo.value === 'ingreso'){
+            // se convierte el valor string a numerico con Number()
+            ingresos.push(new Ingreso(descripcion.value, Number(valor.value)));
+        }else if(tipo.value === 'egreso'){
+            // sintaxis simplificada para convertir string a numero con +
+            egresos.push(new Egreso(descripcion.value, +valor.value));
+        }
+    }
+    cargarApp();
+    // limpiamos los campos
+    descripcion.value = '';
+    valor.value = 0;
 };
